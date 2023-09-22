b9ee223bdbc12e1d8f21f9f5b9f49f338dd47d92
victor <victor@example.com>
2023-09-22 16:17:08 +0800

    v1.2 解决了段错误等小问题

--- src/通讯/server.cpp
+++ src/通讯/server.cpp
@@ -2,7 +2,7 @@
  * @Author: victor victor@example.com
  * @Date: 2023-09-19 18:53:21
  * @LastEditors: victor victor@example.com
- * @LastEditTime: 2023-09-22 16:15:40
+ * @LastEditTime: 2023-09-22 16:17:01
  * @FilePath: \work\stage5\game-project\the-gobang-game-of-cc-md-fk\src\通讯\server.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -207,6 +207,7 @@ Player  way_choose(char *recvbuffer,Player *buff)
     }
     char *message = strtok(NULL,"\0");
     message[strlen(message)-1]='\0';
+
 
     char name[10];
     char password[10]; 
