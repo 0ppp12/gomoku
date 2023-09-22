3671d505b20aed502463041454ee1fdd0fc488d8
victor <victor@example.com>
2023-09-22 17:15:47 +0800

    v1.2

--- src/通讯/server.cpp
+++ src/通讯/server.cpp
@@ -2,7 +2,7 @@
  * @Author: victor victor@example.com
  * @Date: 2023-09-19 18:53:21
  * @LastEditors: victor victor@example.com
- * @LastEditTime: 2023-09-22 17:12:12
+ * @LastEditTime: 2023-09-22 17:14:57
  * @FilePath: \work\stage5\game-project\the-gobang-game-of-cc-md-fk\src\通讯\server.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -148,7 +148,6 @@ int main(void)
                     game_flag=1;
                     cout<<game_flag<<endl;
                 }
-                cout<<game_flag<<endl;
                 if(game_flag==1)
                 {
                     if (rooms.empty()) {
