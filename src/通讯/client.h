af9dec9fc18d1a3ffb256d7a88ef6eb4cb56cce7
pengzzhiyuan <pengzhiyuan@example.com>
2023-09-24 14:55:22 +0800

    客户端通信模块v1.3

--- src/通讯/client.h
+++ src/通讯/client.h
@@ -1,7 +1,7 @@
 /*** 
  * @Author: PENG
  * @Date: 2023-09-22 15:45:02
- * @LastEditTime: 2023-09-24 14:07:25
+ * @LastEditTime: 2023-09-24 14:54:53
  * @LastEditors: PENG
  * @Description: 
  * @FilePath: \c++\c++ 阶段项目\Gobang\src\通讯\client.h
@@ -30,7 +30,8 @@ class Info_SendAndRev{
 public:
     /*初始化TCP连接，参数1为服务器IP，参数2为端口号，返回值为client_socket*/
     int INIT_SOCKET(const char* SERVER_IP,int PORT);
-    /*发送账号密码，参数1为client_socket，参数2：0为注册1为登录，参数3为用户名，参数4为密码*/
+    /*发送账号密码，参数1为client_socket，参数2：0为注册1为登录，参数3为用户名，参数4为密码
+    返回值为字符串"登录失败,账户或密码错误"或"账户已存在"*/
     char * Send_NameAndPassword(int client_socket,int flag,string name,string password);
     /*发送整个棋盘信息到服务器*/
     bool Send_Checkerboard_Info(int client_socket,char a[13][13]);
